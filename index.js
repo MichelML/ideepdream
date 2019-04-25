d9ac187e11f129c0a5826753ee3946a20affbe8d
MichelML <michmoreau.l@gmail.com>
2019-04-25 14:42:16 -0400

    add images add script to upload to google drive

--- index.js
+++ index.js
@@ -4,14 +4,8 @@ const trianglify = require("trianglify");
 const svgToImg = require("svg-to-img");
 const download = require("image-downloader");
 const deepai = require("deepai");
-const { deepaiKey, firebaseConf } = require("./.env");
-
-// firebase
-const firebase = require('firebase/app');
-require('firebase/auth');
-require('firebase/database');
-const app = firebase.initializeApp(firebaseConf);
-
+const { deepaiKey } = require("./.env");
+const getAuthenticatedDrive = require("./google_drive_auth");
 
 deepai.setApiKey(deepaiKey);
 
@@ -42,7 +36,7 @@ const generateImage = async iterations => {
     date.getFullYear().toString() +
     "_" +
     date.getTime().toString();
-  fs.mkdirSync(cleanDate);
+  fs.mkdirSync(`images/${cleanDate}`);
 
   // generate triangle
   const variance = Math.round(Math.random() * 100) / 100;
@@ -51,7 +45,11 @@ const generateImage = async iterations => {
   const allOptions = { ...trianglifyOptions, iterations };
   const svg = trianglify({ variance, seed, x_colors: "random" }).svg();
   svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-  fs.writeFileSync(`${cleanDate}/${cleanDate}.svg`, svg.outerHTML, "utf8");
+  fs.writeFileSync(
+    `images/${cleanDate}/${cleanDate}.svg`,
+    svg.outerHTML,
+    "utf8"
+  );
 
   await svgToImg.from(svg.outerHTML).toJpeg({
     path: "./temp.jpg"
@@ -60,11 +58,11 @@ const generateImage = async iterations => {
   try {
     await deepDreamNest(
       "./temp.jpg",
-      `${cleanDate}/${cleanDate}.jpg`,
+      `images/${cleanDate}/${cleanDate}.jpg`,
       iterations
     );
     fs.writeFileSync(
-      `${cleanDate}/${cleanDate}.txt`,
+      `images/${cleanDate}/${cleanDate}.txt`,
       `
     trianglify variance: ${allOptions.variance}
     trianglify seed: ${allOptions.seed}
@@ -76,14 +74,60 @@ const generateImage = async iterations => {
   } catch (e) {
     console.log(e);
   }
+
+  return {
+    filePath: `images/${cleanDate}/${cleanDate}.jpg`,
+    fileName: `${cleanDate}.jpg`
+  };
+};
+
+const uploadFile = ({ drive, fileName, filePath }) => {
+  return new Promise((resolve, reject) => {
+    var fileMetadata = {
+      name: fileName,
+      parents: ["1I1V-AvpjenXppwHX46dcNNQO1pUhAfDy"]
+    };
+    var media = {
+      mimeType: "image/jpeg",
+      body: fs.createReadStream(filePath)
+    };
+    drive.files.create(
+      {
+        resource: fileMetadata,
+        media: media,
+        fields: "id"
+      },
+      function(err, file) {
+        if (err) {
+          // Handle error
+          console.error(err);
+          reject(err);
+        } else {
+          console.log("File " + fileName + " created.");
+          resolve();
+        }
+      }
+    );
+  });
 };
 
 if (require.main === module) {
   (async () => {
+    // Get authenticated google drive instance
+    const drive = await getAuthenticatedDrive();
+
+    // Generate images
+    let images = [];
     const nImages = parseInt(process.argv[process.argv.length - 1], 10);
     for (let i = 1; i <= nImages; i++) {
       const randomFrom1to4 = Math.floor(Math.random() * 4) + 1;
-      await generateImage(randomFrom1to4);
+      const image = await generateImage(randomFrom1to4);
+      images.push(image);
+    }
+
+    // Upload images to Google Drive
+    for (img of images) {
+      await uploadFile({ drive, ...img });
     }
   })();
 }

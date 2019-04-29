4c338e75b29573b6ba7d9899e9d6098e4054c805
MichelML <michmoreau.l@gmail.com>
2019-04-29 00:35:21 -0400

    avoid the need of catching redirects

--- functions/createBufferPost.js
+++ functions/createBufferPost.js
@@ -2,19 +2,8 @@
 const { buffer } = require("../.env");
 const axios = require("axios");
 const formurlencoded = require("form-urlencoded").default;
-const request = require("request");
 
 (async () => {
-  const redirectLink = await new Promise(resolve => {
-    request.get(
-      "https://docs.google.com/uc?id=1Usj4xeBZ1vtjxtUCgH1ye87Wk4guHPc7",
-      function(err, res, body) {
-        console.log(res.request.uri.href);
-        resolve(res.request.uri.href);
-      }
-    );
-  });
-
   const caption = `caption this
 .⠀⠀
 .⠀⠀
@@ -30,8 +19,8 @@ const request = require("request");
         text: caption,
         profile_ids: [buffer.instagramId],
         media: {
-          picture: redirectLink,
-          thumbnail: redirectLink
+          picture: "https://docs.google.com/uc?id=1Usj4xeBZ1vtjxtUCgH1ye87Wk4guHPc7",
+          thumbnail: "https://docs.google.com/uc?id=1Usj4xeBZ1vtjxtUCgH1ye87Wk4guHPc7"
         }
       })
     )

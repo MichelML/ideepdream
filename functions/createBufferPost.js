1893c5b18df5c24a5017b47b018fef77a59afbde
MichelML <michmoreau.l@gmail.com>
2019-04-29 21:37:36 -0400

    make post appear on top on buffer

--- functions/createBufferPost.js
+++ functions/createBufferPost.js
@@ -24,6 +24,7 @@ module.exports = async function createBufferPost(fileId, drive) {
       }`,
       formurlencoded({
         text: captionGenerator(),
+				top: true,
         profile_ids: [buffer.instagramId],
         media: {
           picture: imageURL,

f3917a22896f4b72de4662005747ca5062fc531e
MichelML <michmoreau.l@gmail.com>
2019-05-01 10:18:02 -0400

    adjust tags

--- functions/captionGenerator.js
+++ functions/captionGenerator.js
@@ -2,20 +2,19 @@ const randomItem = require("./randomItem");
 const _ = require("lodash");
 
 const tags = [
-  "#deepdream #deeplearning #arts #visualarts #visualart #photography #visualartist #javascript #fun #psychedelics #psychedelicarts #nodejs #unsplashphotos #landscape #python #machinelearning #ai #artificialintelligence #abstractpainting #abstractart #drippingart #artists #mindfulness #consciousnessshift #trippy #programming #software #softwareengineering #painting",
-  "#tech #technology #techno #technologies #techie #techy #software #softwareengineer #softwareengineering #artificialintelligence #ai #ml #machinelearning #deeplearning #javascript #nodejs #python #api #deepdream #deep #engineer #engineers #engineering #arts #visualart #photography #visualarts #artificialintelligence #ai #ml #machinelearning #deeplearning",
+  "#deepdream #deeplearning #arts #visualarts #visualart #photography #visualartist #javascript #fun #psychedelics #psychedelicarts #nodejs #unsplashphotos #landscape #python #machinelearning #artificialintelligence #abstractpainting #abstractart #drippingart #artists #mindfulness #consciousnessshift #trippy #programming #software #softwareengineering #painting",
+  "#tech #technology #techno #technologies #techie #techy #software #softwareengineer #softwareengineering #artificialintelligence #machinelearning #deeplearning #javascript #nodejs #python #api #deepdream #deep #engineer #engineers #engineering #arts #visualart #photography #visualarts #artificialintelligence #machinelearning #deeplearning",
   "#psych #psychology #psychedelic #psychedelics #psychic #psychics #psychedelicart #psycho #psychologie #psychotherapie #psychedelica #psychodelicart #psychedelicpainting #psychedelicarts #psychedelicfestival #psychedelicpaint #art #artistoninstagram #artistsoninstagram #artist #arte #artesanato #artsy #artspotlight #arts #artwork #programming #softwareengineering",
-  "#art #artistoninstagram #artistsoninstagram #artist #arte #artesanato #artsy #artspotlight #arts #artwork #artistic #artesanal #artlovers #artofvisuals #artworks #streetart #abstractart #artbasel #artists #deepdream #deeplearning #visualarts #visualart #photography #visualartist #javascript #softwareengineering #artificialintelligence #ai #ml #machinelearning",
-  "#paint #paints #painting #paintings #paintmixing #painter #painteveryday #abstract #abstractart #abstractarts #abstract_art #abstract_arts #painttoolsai #paintingoftheday #originalpainting #paintlife #digitalpainting #paintingart #paintersofinstagram #intuitivepainting #digitalpaint #abstractartwork #artificialintelligence #ai #ml #machinelearning #deeplearning",
+  "#art #artistoninstagram #artistsoninstagram #artist #arte #artesanato #artsy #artspotlight #arts #artwork #artistic #artesanal #artlovers #artofvisuals #artworks #streetart #abstractart #artbasel #artists #deepdream #deeplearning #visualarts #visualart #photography #visualartist #javascript #softwareengineering #artificialintelligence #machinelearning",
+  "#paint #paints #painting #paintings #paintmixing #painter #painteveryday #abstract #abstractart #abstractarts #abstract_art #abstract_arts #painttoolsai #paintingoftheday #originalpainting #paintlife #digitalpainting #paintingart #paintersofinstagram #intuitivepainting #digitalpaint #abstractartwork #artificialintelligence #machinelearning #deeplearning",
 ];
 
-// module.exports = function captionGenerator() {
-//   return `caption
-//   .
-//   .
-//   ${randomItem(tags)}`;
-// };
-tags.forEach((h) => console.log(_.uniq(h.split(" ")).join(" ")));
+module.exports = function captionGenerator() {
+  return `I Deep Dream.
+  .
+  .
+  ${randomItem(tags)}`;
+};
 
 /**
  * Tags sequence generator, uncomment to generate additional ones
